bitcoin-green/bitcoin-green-lib

Fix/deprecated buffer usage (#105)
* Fixed buffer usage in ecdsa, signature and message classes

* Fix buffer usage in `BufferWriter`

* Fix buffer usage in `PrivateKey`

* Fix buffer usage in `Address`

* Fix buffer usage in `HDPubKey`

* Fix buffer usage in `HDPrivateKey`

* Fix buffer usage in `Opcode`

* Fix buffer usage in `PublicKey`

* Fix buffer usage in `util/buffer`

* Fix buffer usage in `MerkleBlock`

* Fix buffer usage in `Block`

* Fix buffer usage in `BlockHeader`

* Fix buffer usage in `Point`

* Fix buffer usage in `Hash`

* Fix buffer usage in `BN`

* Fix buffer usage in `Random`

* Fix buffer usage in `Interpreter`

* Fix buffer usage in `Script`

* Fix buffer usage in `BufferReader` and `base58check`

* Fix buffer usage in `VarInt`

* Fix buffer usage in `Transaction/Signature` and `GovObject`

* Fix buffer usage in `Mnemonic`

* Fix buffer usage in `pbkdf2`

* Fix buffer usage in `SigHash`

* Fix buffer usage in `HashSignature`

* Remove usages of `buffer` module

* Remove `buffer-compare` module

* Fix Buffer usage in tests

* Remove `buffer` module from tests

* Remove unused sha module

* Bump a version

* Fix buffer usage in docs

test/networks.js
@@ -20,13 +20,13 @@ describe('Networks', function() {
 
   it('will enable/disable regtest Network', function() {
     networks.enableRegtest();
-    networks.testnet.networkMagic.should.deep.equal(new Buffer('fcc1b7dc', 'hex'));
+    networks.testnet.networkMagic.should.deep.equal(Buffer.from('fcc1b7dc', 'hex'));
     networks.testnet.port.should.equal(19994);
     networks.testnet.dnsSeeds.should.deep.equal([]);
     networks.testnet.regtestEnabled.should.equal(true);
 
     networks.disableRegtest();
-    networks.testnet.networkMagic.should.deep.equal(new Buffer('cee2caff', 'hex'));
+    networks.testnet.networkMagic.should.deep.equal(Buffer.from('cee2caff', 'hex'));
     networks.testnet.port.should.equal(19999);
     networks.testnet.dnsSeeds.should.deep.equal([
      'testnet-seed.darkcoin.io',
@@ -62,7 +62,7 @@ describe('Networks', function() {
       if (key !== 'networkMagic') {
         customnet[key].should.equal(custom[key]);
       } else {
-        var expected = new Buffer('e7beb4d4', 'hex');
+        var expected = Buffer.from('e7beb4d4', 'hex');
         customnet[key].should.deep.equal(expected);
       }
     }

stashpayio/stashcore-lib

test/networks.js
@@ -17,12 +17,12 @@ describe('Networks', function() {
 
   it('will enable/disable regtest Network', function() {
     networks.enableRegtest();
-    networks.testnet.networkMagic.should.deep.equal(new Buffer('f6cfb1d8', 'hex'));
+    networks.testnet.networkMagic.should.deep.equal(Buffer.from('f6cfb1d8', 'hex'));
     networks.testnet.port.should.equal(19994);
     networks.testnet.dnsSeeds.should.deep.equal([]);
     networks.testnet.regtestEnabled.should.equal(true);
     networks.disableRegtest();
-    networks.testnet.networkMagic.should.deep.equal(new Buffer('efa2faf7', 'hex'));
+    networks.testnet.networkMagic.should.deep.equal(Buffer.from('efa2faf7', 'hex'));
     networks.testnet.port.should.equal(19999);
     networks.testnet.dnsSeeds.should.deep.equal([
       'testseed1.stashpay.io'
@@ -56,7 +56,7 @@ describe('Networks', function() {
       if (key !== 'networkMagic') {
         customnet[key].should.equal(custom[key]);
       } else {
-        var expected = new Buffer('e7beb4d4', 'hex');
+        var expected = Buffer.from('e7beb4d4', 'hex');
         customnet[key].should.deep.equal(expected);
       }
     }